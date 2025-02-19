4112070d7cf726d3d32bc690d966494bcee24d61
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 13:29:12 -0800

    Implemented classes into main

--- COMP003A.LectureActivity6-1/Program.cs
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -8,7 +8,14 @@ namespace COMP003A.LectureActivity6_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create vehicle with brand and year
+            Vehicle myVehicle = new Vehicle() { Brand = "Toyota", Year = 2020 };
+            // Create A Car with brand, year, and number of doors
+            Car myCar = new Car("Honda", 2022, 4);
+            // Display vehicle and car details.
+            myVehicle.DisplayInfo();
+            myCar.DisplayInfo();
+            
         }
     }
 }
