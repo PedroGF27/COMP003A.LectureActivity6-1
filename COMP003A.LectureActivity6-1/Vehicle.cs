0e2fa7d6bbd2af1874f9ebd8f0f6c848ea59c321
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 13:13:47 -0800

    Made vehicle class

---
+++ COMP003A.LectureActivity6-1/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_1
+{
+    /// <summary>
+    /// Represent vehicle
+    /// </summary>
+    class Vehicle
+    {
+        // Fields
+        private string _brand;
+
+        // properties
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public int  Year { get; set; }
+
+        /// <summary>
+        /// Display vehicle details.
+        /// </summary>
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Year: {Year}");
+        }
+    }
+}
